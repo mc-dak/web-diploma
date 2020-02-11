b717d27e5f4b34eae3c507cb573955dfbb9a3a9a
dak <dkubrakov@bk.ru>
2020-02-11 17:03:31 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+//script for swipe
+new Swiper('.slider', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 2
+    },
+    1025: {
+      slidesPerView: 3,
+      spaceBetween: 27
+    }
+  },
+  loop: true,
+  wrapperClass: 'slider__list',
+  slideClass: 'slider__item',
+  pagination: {
+    el: '.slider__pagination',
+    type: 'bullets',
+    bulletClass: 'paginator__item',
+    bulletActiveClass: 'paginator__item--active',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.slider__button--next',
+    prevEl: '.slider__button--prev',
+  },
+});
+
+//script for popup
+$(function () {
+  $('button.show_popup').click(function () {
+    $('div.'+$(this).attr("rel")).fadeIn(500);
+    $("body").append("<div id='overlay'></div>");
+    $('#overlay').show().css({'filter' : 'alpha(opacity=80)'});
+    return false;       
+  }); 
+  $('a.close').click(function () {
+    $(this).parent().fadeOut(100);
+    $('#overlay').remove('#overlay');
+    return false;
+  });
+}); 
+
+//script for burger
+    let burger = document.querySelector('.menu-burger');
+    let menu = document.querySelector('.menu');
+    let close = document.querySelector('.menu-burger_close');
+
+    burger.addEventListener('click', function(e){
+      e.preventDefault();
+      menu.classList.add('menu-burger_open');
+    });
+
+    close.addEventListener('click', function(e){
+      e.preventDefault();
+      menu.classList.remove('menu-burger_open');
+    });
